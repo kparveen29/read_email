5c04fec0e5817bc18c95c3e93d6bf9de9de35b76
kparveen29 <91961871+kparveen29@users.noreply.github.com>
2021-11-17 12:17:12 +0530

    Add files via upload

---
+++ read_email.py
@@ -0,0 +1,59 @@
+import imaplib
+import email
+import os
+
+user = 'pkrahulkumar2041@gmail.com'
+password = 'ASDF1234#'
+imap_url = 'imap.gmail.com'
+
+
+def auth(user, password, imap_url):
+    con = imaplib.IMAP4_SSL(imap_url)
+    con.login(user, password)
+    return con
+# extracts the body from the email
+
+
+def get_body(msg):
+    if msg.is_multipart():
+        return get_body(msg.get_payload(0))
+    else:
+        return msg.get_payload(None, True)
+# allow to download attachments
+
+
+def get_attachments(msg):
+    for part in msg.walk():
+        if part.get_content_maintype() == 'multipart':
+            continue
+        if part.get('Content-Disposition') is None:
+            continue
+        fileName = part.get_filename()
+
+        if bool(fileName):
+            filePath = os.path.join(attachment_dir, fileName)
+            with open(filePath, 'wb') as f:
+                f.write(part.get_payload(decode=True))
+# search for a particular email
+
+
+def search(key, value, con):
+    result, data = con.search(None, key, '"{}"'.format(value))
+    return data
+# extracts emails from byte array
+
+
+def get_emails(result_bytes):
+    msgs = []
+    for num in result_bytes[0].split():
+        typ, data = con.fetch(num, '(RFC822)')
+        msgs.append(data)
+    return msgs
+
+
+con = auth(user, password, imap_url)
+con.select('INBOX')
+
+result, data = con.fetch(b'10', '(RFC822)')
+raw = email.message_from_bytes(data[0][1])
+get_attachments(raw)
